2cd0753d912537954c36fe73aeb695d899390d4b
DSR! <xchwarze@gmail.com>
2021-12-18 14:42:32 -0300

    Shellcheck fixs + minor refactor

--- autossh/rootfs/usr/bin/autossh.sh
+++ autossh/rootfs/usr/bin/autossh.sh
@@ -3,20 +3,34 @@
 # Home Assistant Community Add-on: Autossh
 # Autossh implementation logic
 # ==============================================================================
-declare SSH_KEY_PATH=/data/ssh
-declare SSH_KEY_NAME=$(bashio::config 'ssh_key_name')
-declare HOSTNAME=$(bashio::config 'hostname')
-declare SSH_PORT=$(bashio::config 'ssh_port')
-declare USERNAME=$(bashio::config 'username')
-declare REMOTE_FORWARDING=$(bashio::config 'remote_forwarding')
-declare LOCAL_FORWARDING=$(bashio::config 'local_forwarding')
-declare OTHER_SSH_OPTIONS=$(bashio::config 'other_ssh_options')
-declare MONITOR_PORT=$(bashio::config 'monitor_port')
-declare SERVER_ALIVE_INTERVAL=$(bashio::config 'server_alive_interval')
-declare SERVER_ALIVE_COUNT_MAX=$(bashio::config 'server_alive_count_max')
-declare GATETIME=$(bashio::config 'gatetime')
-declare RETRY_INTERVAL=$(bashio::config 'retry_interval')
+declare SSH_KEY_PATH
+declare SSH_KEY_NAME
+declare HOSTNAME
+declare SSH_PORT
+declare USERNAME
+declare REMOTE_FORWARDING
+declare LOCAL_FORWARDING
+declare OTHER_SSH_OPTIONS
+declare MONITOR_PORT
+declare SERVER_ALIVE_INTERVAL
+declare SERVER_ALIVE_COUNT_MAX
+declare GATETIME
+declare RETRY_INTERVAL
+declare -a autossh_params
 
+SSH_KEY_PATH=/data/ssh
+SSH_KEY_NAME=$(bashio::config 'ssh_key_name')
+HOSTNAME=$(bashio::config 'hostname')
+SSH_PORT=$(bashio::config 'ssh_port')
+USERNAME=$(bashio::config 'username')
+REMOTE_FORWARDING=$(bashio::config 'remote_forwarding')
+LOCAL_FORWARDING=$(bashio::config 'local_forwarding')
+OTHER_SSH_OPTIONS=$(bashio::config 'other_ssh_options')
+MONITOR_PORT=$(bashio::config 'monitor_port')
+SERVER_ALIVE_INTERVAL=$(bashio::config 'server_alive_interval')
+SERVER_ALIVE_COUNT_MAX=$(bashio::config 'server_alive_count_max')
+GATETIME=$(bashio::config 'gatetime')
+RETRY_INTERVAL=$(bashio::config 'retry_interval')
 
 # Set AUTOSSH global variables
 export AUTOSSH_GATETIME=$GATETIME
@@ -46,40 +60,41 @@ ssh-keyscan -p $SSH_PORT $HOSTNAME > /etc/ssh/ssh_known_hosts
 # autossh params
 # https://www.harding.motd.ca/autossh/
 # https://linux.die.net/man/1/autossh
-command_args="-M ${MONITOR_PORT} \
-    -N -q \
-    -o ExitOnForwardFailure=yes \
-    -o ServerAliveInterval=${SERVER_ALIVE_INTERVAL} \
-    -o ServerAliveCountMax=${SERVER_ALIVE_COUNT_MAX} \
-    ${USERNAME}@${HOSTNAME} \
-    -p ${SSH_PORT} \
-    -i ${SSH_KEY_PATH}/${SSH_KEY_NAME}"
+autossh_params+=(-M ${MONITOR_PORT})
+autossh_params+=(-N -q)
+autossh_params+=(-o ExitOnForwardFailure=yes)
+autossh_params+=(-o ServerAliveInterval=${SERVER_ALIVE_INTERVAL})
+autossh_params+=(-o ServerAliveCountMax=${SERVER_ALIVE_COUNT_MAX})
+autossh_params+=(${USERNAME}@${HOSTNAME})
+autossh_params+=(-p ${SSH_PORT})
+autossh_params+=(-i ${SSH_KEY_PATH}/${SSH_KEY_NAME})
 
 if [ -n "$REMOTE_FORWARDING" ]; then
     bashio::log.debug "Processing remote_forwarding argument '$REMOTE_FORWARDING'"
     while read -r line; do
-        command_args="${command_args} -R $line"
+        autossh_params+=(-R $line)
     done <<< "$REMOTE_FORWARDING"
 fi
 
 if [ -n "$LOCAL_FORWARDING" ]; then
     bashio::log.debug "Processing local_forwarding argument '$LOCAL_FORWARDING'"
     while read -r line; do
-        command_args="${command_args} -L $line"
+        autossh_params+=(-L $line)
     done <<< "$LOCAL_FORWARDING"
 fi
 
 # Enable debug mode on autossh
 if bashio::debug; then
-    command_args="${command_args} -v"
+    autossh_params+=(-v)
 fi
 
-command_args="${command_args} ${OTHER_SSH_OPTIONS}"
+autossh_params+=(${OTHER_SSH_OPTIONS})
+
 bashio::log.info "Autossh start!"
-bashio::log.debug "Executing autossh with params: ${command_args}"
+bashio::log.debug "Executing autossh with params: ${autossh_params}"
 
 # Start autossh
-until /usr/bin/autossh ${command_args} 2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
+until /usr/bin/autossh ${autossh_params} 2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
 do
     bashio::log.info "Failed, retrying in ${RETRY_INTERVAL}s"
     sleep ${RETRY_INTERVAL}
